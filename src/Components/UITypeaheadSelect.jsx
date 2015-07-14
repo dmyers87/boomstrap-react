77b46eba0436f7956ac09eedba03d4cfd60e8d17
Mark Funk <mark@boomtownroi.com>
2015-07-14 10:09:10 -0400

    Adding example, docs, and removing deprecation usage from the UITypeaheadSelect

--- src/Components/UITypeaheadSelect.jsx
+++ src/Components/UITypeaheadSelect.jsx
@@ -1,79 +1,113 @@
 const React = require('react/addons');
 const cx    = require('classnames');
 
-const OverlayTrigger           = require('react-bootstrap').OverlayTrigger;
+const { Overlay }              = require('react-bootstrap');
 const UITypeaheadSelectOverlay = require('./UITypeaheadSelectOverlay');
 
 module.exports = React.createClass({
   displayName: 'UI Typeahead Select',
 
   propTypes: {
+    /**
+     * Class for Container of Typeahead
+     */
     className:     React.PropTypes.string,
+
+    /**
+     * Class for Icon in input
+     */
     iconClass:     React.PropTypes.string,
+
+    /**
+     * Class for input
+     */
     inputClass:    React.PropTypes.string,
+
+    /**
+     * Placeholder text for input
+     */
     placeholder:   React.PropTypes.string,
+
+    /**
+     * Function to call when the user types in the input
+     */
     onSearch:      React.PropTypes.func,
+
+    /**
+     * Function to call when the user selects an item in the dropdown
+     */
     onSelectMatch: React.PropTypes.func,
+
+    /**
+     * Array of options to display to the user
+     */
     options:       React.PropTypes.arrayOf(
       React.PropTypes.shape({
-        element: React.PropTypes.element,
+        element: React.PropTypes.node,
         payload: React.PropTypes.any
       })
     ),
+
+    /**
+     * Indicates this typeahead is on a modal and must be a higher zIndex
+     */
     overlayModal:  React.PropTypes.bool
+  },
+
+  getDefaultProps() {
+    return {
+      className:     '',
+      iconClass:     '',
+      inputClass:    '',
+      placeholder:   '',
+      onSearch:      () => {},
+      onSelectMatch: () => {},
+      options:       [],
+      overlayModal:  false
+    };
   },
 
   getInitialState() {
     return {
-      searchText: '',
-      searchIndex: 0,
-      searchLeft: 0
+      searchText:   '',
+      searchIndex:  0,
+      searchLeft:   0,
+      overlayShown: false
     };
   },
 
-  _updateSearchLeft(callback = () => {}) {
-    const node            = React.findDOMNode(this);
-    const nodeBox         = node.getBoundingClientRect();
-    const documentElement = document.documentElement;
-    const searchLeft = nodeBox.left + window.pageXOffset - documentElement.clientLeft;
-    if (searchLeft !== this.state.searchLeft) {
-      this.setState({ searchLeft }, callback);
-    } else {
-      callback();
+  keyHandlers: {
+    Enter(/* e */) {
+      if (this.props.options.length) {
+        this._selectMatch(this.props.options[this.state.searchIndex].payload);
+      }
+    },
+
+    ArrowDown(/* e */) {
+      let searchIndex = 0;
+      if (this.state.searchIndex !== this.props.options.length - 1) {
+        searchIndex = this.state.searchIndex + 1;
+      }
+
+      this.setState({ searchIndex });
+    },
+
+    ArrowUp(/* e */) {
+      let searchIndex;
+      if (this.state.searchIndex !== 0) {
+        searchIndex = this.state.searchIndex - 1;
+      } else {
+        searchIndex = Math.max(this.props.options.length - 1, 0);
+      }
+
+      this.setState({ searchIndex });
     }
   },
 
   _onKeyDown(e) {
-    if (e.key === 'Enter' || e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+    if (this.keyHandlers[e.key]) {
       e.preventDefault();
-    }
-
-    if (e.key === 'Enter') {
-      if (this.props.options &&  this.props.options.length) {
-        this._selectMatch(this.props.options[this.state.searchIndex].payload);
-      }
-      return;
-    }
-
-    let newSearchIndex = 0;
-    if (e.key === 'ArrowDown') {
-      if (this.state.searchIndex !== this.props.options.length - 1) {
-        newSearchIndex = this.state.searchIndex + 1;
-      }
-
-      this.setState({
-        searchIndex: newSearchIndex
-      });
-    } else if (e.key === 'ArrowUp') {
-      if (this.state.searchIndex !== 0) {
-        newSearchIndex = this.state.searchIndex - 1;
-      } else {
-        newSearchIndex = this.props.options.length - 1;
-      }
-
-      this.setState({
-        searchIndex: newSearchIndex
-      });
+      this.keyHandlers[e.key].bind(this)(e);
     }
   },
 
@@ -92,56 +126,62 @@ module.exports = React.createClass({
   },
 
   _selectMatch(payload) {
-    // Blur input to close search
-    this.refs.overlay.getOverlayDOMNode().blur();
-
     this.setState({
-      searchText: ''
+      searchText: '',
+      overlayShown: false
     }, () => {
       this.props.onSelectMatch(payload);
       this.props.onSearch(this.state.searchText);
+      React.findDOMNode(this.refs.searchInput).blur();
     });
   },
 
   _onFocus() {
-    this._updateSearchLeft(() => {
-      this.refs.overlay.show();
+    const node            = React.findDOMNode(this);
+    const nodeBox         = node.getBoundingClientRect();
+    const documentElement = document.documentElement;
+    const searchLeft = nodeBox.left + window.pageXOffset - documentElement.clientLeft;
+    this.setState({
+      searchLeft,
+      overlayShown: true
     });
   },
 
   _onBlur() {
-    this.refs.overlay.hide();
+    this.setState({
+      overlayShown: false
+    });
   },
 
   render() {
     const searchIcon = cx('ficon ficon-search', this.props.iconClass);
-
-    const inputClass = cx('form-control', this.props.inputClass);
+    const inputClass = cx('form-control',       this.props.inputClass);
 
     return (
       <div className={this.props.className}>
-        <OverlayTrigger ref='overlay' trigger='manual'
-          defaultOverlayShown={false}
-          overlay={
-            <UITypeaheadSelectOverlay
-              positionLeftOverride={this.state.searchLeft}
-              searchIndex={this.state.searchIndex}
-              selectActive={this._selectActive}
-              selectMatch={this._selectMatch}
-              options={this.props.options}
-              overlayModal={this.props.overlayModal}/>
-          }
-          placement='bottom'>
-          <input
-            onFocus={this._onFocus} onBlur={this._onBlur}
-            autoComplete='off' type='text'
-            className={inputClass}
-            placeholder={this.props.placeholder}
-            onKeyDown={this._onKeyDown}
-            onChange={this._onChange}
-            value={this.state.searchText}/>
-        </OverlayTrigger>
+        <input
+          ref='searchInput'
+          onFocus={this._onFocus}
+          onBlur={this._onBlur}
+          autoComplete='off' type='text'
+          className={inputClass}
+          placeholder={this.props.placeholder}
+          onKeyDown={this._onKeyDown}
+          onChange={this._onChange}
+          value={this.state.searchText}/>
         <i className={searchIcon}/>
+        <Overlay
+          show={this.state.overlayShown}
+          target={() => React.findDOMNode(this)}
+          placement='bottom'>
+          <UITypeaheadSelectOverlay
+            positionLeftOverride={this.state.searchLeft}
+            searchIndex={this.state.searchIndex}
+            selectActive={this._selectActive}
+            selectMatch={this._selectMatch}
+            options={this.props.options}
+            overlayModal={this.props.overlayModal}/>
+        </Overlay>
       </div>
     );
   }
